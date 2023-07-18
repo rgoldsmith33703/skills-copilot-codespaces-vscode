03c5a25b53d19c4b481f35db5eec885dad6cb1ad
rgoldsmith33703 <110078063+rgoldsmith33703@users.noreply.github.com>
2023-07-18 06:05:45 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,3 +1,12 @@
 // create web server
 
+const express = require('express')
+const router = express.Router()
+const commentController = require('../controllers/commentController')
+
+router.get('/', commentController.comment_list)
+router.get('/create', commentController.comment_create_post)
+router.post('/create', commentController.comment_create_post)
+router.get('/:id/delete', commentController.comment_delete)
+
 
